9f5a2f2817b75755e039bcfbdffdd49ad35c70c8
jk960903 <jk960903@naver.com>
2021-12-25 02:07:02 +0900

    Spring Projectinitalize

---
+++ backend/src/main/java/com/example/backswim/BackswimApplication.java
@@ -0,0 +1,13 @@
+package com.example.backswim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackswimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackswimApplication.class, args);
+    }
+
+}
