1bc0d22a5b5b47daf3eafc8dcc911475397e985a
HyeongJun94 <37447578+HyeongJun94@users.noreply.github.com>
2022-02-16 21:07:20 +0900

    [BAC-29]: [Front][Pool] header 컴포넌트 (#35)
    [BAC-29]: [Front][Pool] header 컴포넌트
    
    Jira Number: BAC-29
    
    Description:
    - puppetter 설정
    - header 컴포넌트 렌더링 unit test
    
    Unit test:
    PASS src/components/header/header.test.js
      [Pool] BAC-29 헤더 컴포넌트
        비로그인 경우
          ✓ 로그인 버튼 렌더링 (24 ms)
          ✓ 회원가입 버튼 렌더링 (4 ms)
        로그인 경우
          ✓ 마이 페이지 버튼 렌더링 (3 ms)

--- frontend/src/components/header/header.test.js
+++ frontend/src/components/header/header.test.js
@@ -3,54 +3,38 @@
  */
 
 import React from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
-import { act } from 'react-dom/test-utils';
-import pretty from 'pretty';
+import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
+
 import Header from './header.js';
 
-let container = null;
-beforeEach(() => {
-  container = document.createElement('div');
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
-
 describe('[Pool] BAC-29 헤더 컴포넌트', () => {
-  test('SnapShot test', () => {
-    act(() => {
-      render(
-        <BrowserRouter>
-          <Header isLogin={true}></Header>
-        </BrowserRouter>,
-        container
-      );
-    });
-
-    expect(pretty(container.innerHTML)).toMatchInlineSnapshot(`
-"<div class=\\"header\\"><a class=\\"header-logo-link\\" href=\\"/\\"><i class=\\"header-logo fas fa-swimmer\\"></i></a>
-  <nav class=\\"header-item-container\\"><a id=\\"btn-myPage\\" class=\\"item\\" href=\\"/myPage\\">My Page</a></nav>
-</div>"
-`);
-
-    act(() => {
+  describe('비로그인 경우', () => {
+    beforeEach(() => {
       render(
         <BrowserRouter>
           <Header isLogin={false}></Header>
-        </BrowserRouter>,
-        container
+        </BrowserRouter>
       );
     });
+    test('로그인 버튼 렌더링', () => {
+      expect(screen.getByText(/Sign In/)).toBeInTheDocument();
+    });
+    test('회원가입 버튼 렌더링', () => {
+      expect(screen.getByText(/Sign Up/)).toBeInTheDocument();
+    })
+  });
 
-    expect(pretty(container.innerHTML)).toMatchInlineSnapshot(`
-"<div class=\\"header\\"><a class=\\"header-logo-link\\" href=\\"/\\"><i class=\\"header-logo fas fa-swimmer\\"></i></a>
-  <nav class=\\"header-item-container\\"><a id=\\"btn-signIn\\" class=\\"item\\" href=\\"/signIn\\">Sign In</a><a id=\\"btn-signUp\\" class=\\"item\\" href=\\"/signUp\\">Sign Up</a></nav>
-</div>"
-`);
+  describe('로그인 경우', () => {
+    beforeEach(() => {
+      render(
+        <BrowserRouter>
+          <Header isLogin={true}></Header>
+        </BrowserRouter>
+      );
+    });
+    test('마이 페이지 버튼 렌더링', () => {
+      expect(screen.getByText(/My Page/)).toBeInTheDocument();
+    });
   });
 });
