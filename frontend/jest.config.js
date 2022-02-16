c21a6b9037ca45c416ca298e422702036706f33c
HyeongJun94 <37447578+HyeongJun94@users.noreply.github.com>
2022-02-16 21:07:20 +0900

    [BAC-29]: [Front][Pool] header 컴포넌트 (#35)
    [BAC-29]: [Front][Pool] header 컴포넌트
    
    Jira Number: BAC-29
    
    Description:
    - puppetter 설정
    - header 컴포넌트 렌더링 unit test
    
    Unit test:
    PASS src/components/header/header.test.js
      [Pool] BAC-29 헤더 컴포넌트
        비로그인 경우
          ✓ 로그인 버튼 렌더링 (24 ms)
          ✓ 회원가입 버튼 렌더링 (4 ms)
        로그인 경우
          ✓ 마이 페이지 버튼 렌더링 (3 ms)

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -5,4 +5,6 @@ module.exports = {
     '#common/(.*)$': '<rootDir>/src/common/$1',
     '#utils/(.*)$': '<rootDir>/src/utils/$1',
   },
+  // 각각의 테스트 파일을 실행하기 전에 configure 또는 setup 하는 module의 path
+  setupFilesAfterEnv: ['./src/setupTests.js'],
 };
